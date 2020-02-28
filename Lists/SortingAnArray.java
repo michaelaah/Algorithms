4c8fde7b63691fc334f3089aa1142f8c1c103e2a
michaelaah <55564227+michaelaah@users.noreply.github.com>
2020-02-28 14:06:08 -0400

    Update SortingAnArray.java
    Added source code and comments

--- Lists/SortingAnArray.java
+++ Lists/SortingAnArray.java
@@ -1,1 +1,39 @@
 
+
+/**
+ * The algorithm below sorts an integer array.
+ */
+public class SortingAnArray {
+
+    public static void main(String[] args) {
+        
+        // Instantiating and populating an int array.
+        int[] array = {6,4,2,5,0,1,3};
+      
+        // Print out current state of the array to the console.
+        System.out.print("Current Array: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.print("\n");
+
+        // Below is a sorting algorithm.
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if(array[i] > array[j]){
+                    int tempVar = array[i];
+                    array[i] = array[j];
+                    array[j] = tempVar;
+                }
+            }
+        }
+        
+        // Printing out te array after running the sorting algorithm.
+        System.out.print("Sorted Array: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.print("\n");
+
+    }
+}
