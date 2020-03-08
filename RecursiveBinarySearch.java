d28dba65354b56ec3003fb9b52eb84ea1c1d46d2
michaelaah <55564227+michaelaah@users.noreply.github.com>
2020-03-08 16:36:53 -0300

    Update RecursiveBinarySearch.java
    Copy pasted source code

--- RecursiveBinarySearch.java
+++ RecursiveBinarySearch.java
@@ -1,1 +1,89 @@
+/**
+ * Below is an algorithm to perform Binary search on a sorted array of integers.
+ * Note: the binary search algorithm is within the BinarySearch method, the
+ * main method below contains test cases.
+ */
+public class Main {
 
+    public static void main(String[] args) {
+
+        // Instantiate an integer array.
+        int[] array = new int[100];
+
+        // Filling array with numbers 0 through, and including, 99.
+        for (int i = 0; i < array.length ; i++) {
+            array[i] = i;
+        }
+
+        /*
+         * Declaring and assigning the low and high variables to be used
+         * with each BinarySearch method call.
+         */
+        int low = 0;
+        int high = array.length;
+
+        /*
+         * Several test cases follow below where the element being searched
+         * for changes - meaning the target variable is assigned to new values
+         * with each BinarySearch method call.
+         */
+        int target = 0;
+        System.out.println(BinarySearch(array,low,high,target));
+        System.out.println("Expected: true");
+
+         target = 181;
+        System.out.println(BinarySearch(array,low,high,target));
+        System.out.println("Expected: false");
+
+        target = 9;
+        System.out.println(BinarySearch(array,low,high,target));
+        System.out.println("Expected: true");
+
+        target = -12;
+        System.out.println(BinarySearch(array,low,high,target));
+        System.out.println("Expected: false");
+
+        target = 89;
+        System.out.println(BinarySearch(array,low,high,target));
+        System.out.println("Expected: true");
+
+        target = 99;
+        System.out.println(BinarySearch(array,low,high,target));
+        System.out.println("Expected: true");
+
+        target = 100;
+        System.out.println(BinarySearch(array,low,high,target));
+        System.out.println("Expected: false");
+
+    }
+
+    /**
+     * 
+     * @param array
+     * @param low
+     * @param high
+     * @param target
+     * @return
+     */
+    public static boolean BinarySearch(int[] array, int low, int high, int target){
+        if (high >= low){
+            int mid = (high + low) / 2;
+
+            if (mid > array.length - 1){
+                return false;
+            }
+            if (array[mid] == target){
+                return true;
+            }
+            if (array[mid] > target){
+
+                return BinarySearch(array,low,--mid,target);
+            } else {
+
+                return BinarySearch(array,++mid,high,target);
+            }
+        } else {
+            return false;
+        }
+    }
+}
