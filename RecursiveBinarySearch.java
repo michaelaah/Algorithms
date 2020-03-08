c7c5fc4b3465fbc7e25514645e0628960c5c41fa
michaelaah <55564227+michaelaah@users.noreply.github.com>
2020-03-08 16:45:27 -0300

    Update RecursiveBinarySearch.java
    added method comments

--- RecursiveBinarySearch.java
+++ RecursiveBinarySearch.java
@@ -3,7 +3,7 @@
  * Note: the binary search algorithm is within the BinarySearch method, the
  * main method below contains test cases.
  */
-public class Main {
+public class RecursiveBinarySearch {
 
     public static void main(String[] args) {
 
@@ -67,24 +67,28 @@ public class Main {
      * @return true of the element is found within the array, else false
      */
     public static boolean BinarySearch(int[] array, int low, int high, int target){
-        if (high >= low){
-            int mid = (high + low) / 2;
+        if (high >= low){                         // If high is above or equal to low
+            int mid = (high + low) / 2;           // Calculate the mid variable
 
-            if (mid > array.length - 1){
-                return false;
+            // If the calculated mid is out of the array's bounds
+            if (mid > array.length - 1){ 
+                return false;                      // return false
             }
-            if (array[mid] == target){
-                return true;
+            if (array[mid] == target){  // If the mid of the array equals the target integer
+                return true;                       // return true
             }
-            if (array[mid] > target){
+            if (array[mid] > target){  // If mid of the array is greater than target integer
 
-                return BinarySearch(array,low,--mid,target);
-            } else {
-
+                // Recursive call with mid entered as the high parameter variable
+                return BinarySearch(array,low,--mid,target);    
+            } else {            // Else, mid of the array is less than the target integer
+                
+                // Recursive call with mid entered as the low parameter variable
                 return BinarySearch(array,++mid,high,target);
             }
-        } else {
-            return false;
+        } else {        // Else, high and low have crossed and the target has not been found
+            
+            return false;                       // return false
         }
     }
 }
